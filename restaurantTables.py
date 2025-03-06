73449052cf5b5eb7601b9ea4220035aa979e9133
Ngoc <ngockhanhmai1102@gmail.com>
2025-03-06 02:02:50 +0000

    finish code

--- restaurantTables.py
+++ restaurantTables.py
@@ -51,26 +51,47 @@ def all_available_row_by_key(key, dict): # Return all the rows with available se
     return available_rows
 
 # -------------------------------------------------------------------------------------
+# FOR FUN
+# To print a new copy of the original tables with updated keys 
+def adjust_the_layout(available_tables, layout): # ['T1(2) - 2', 'T2(4) - 1', 'T4(6) - 1']
+    new_layout = layout.copy()
+    for table in available_tables.copy():
+        try: 
+            row = int(table.split()[-1])
+            table_ID = table[:5] # Only take the ID e.g. T1(2) - 2 ---> T1(2)
+            col = layout[0].index(table_ID)
+            new_layout[row][col] = '*'
+        except ValueError: # For the combine tables where the format is different
+            new_tables = table.split(' ')[:7] # Split the table and only take the first 7 parts of the splitted list 
+            '''
+            example: 
+                table = 'T4(6) - 6 and T5(4) - 6 could seat 10 peoples'
+                splitted_table = ['T4(6)', '-', '6', 'and', 'T5(4)', '-', '6', 'could', 'seat', '10', 'peoples']
+                new_tables = ['T4(6)', '-', '6', 'and', 'T5(4)', '-', '6'] --> only show the [:7] part        
+            '''
+            new_tables = (" ").join(new_tables)
+            new_tables = new_tables.split(' and ')
+            for table2 in new_tables:
+                row = int(table2.split()[-1])
+                table_ID = table[:5]
+                col = layout[0].index(table_ID)
+                new_layout[row][col] = '-'
+                new_layout[row][col+1] = '-'
+    return new_layout
 
 # LEVEL 4
 # Note: Adjacent table are only the tables in the same row
-def find_fit_table_adjacent(layout, party_size, available_tables = None): # Check if adjacent tables are available (ONLY use when normal tables cant fit)
+def find_fit_table_adjacent(layout, party_size): # Check if adjacent tables are available (ONLY use when normal tables cant fit)
     capacity = table_capacity(layout)
     combined_tables = []
     keys = list(capacity.keys())
     caps = list(capacity.values())
-    breakpoint()
     for i in range(len(caps) - 1):
         available_rows = all_available_row_by_key(keys[i], layout)
-        if available_tables != None:
-            for table in available_tables:
-                if keys[i] == table[:5]: # if its already available as a individual table
-                    keys.pop(i) # Remove that key 
         for row in available_rows:
             col = layout[0].index(keys[i])
-            if caps[i] + caps[i+1] >= party_size and layout[row][col] == 'o' and layout[row][col + 1] == 'o':
+            if caps[i] + caps[i+1] >= party_size and caps[i] < party_size and layout[row][col] == 'o' and layout[row][col + 1] == 'o':
                 combined_tables.append(f"{keys[i]} - {row} and {keys[i+1]} - {row} could seat {caps[i] + caps[i+1]} peoples")
-    print(f"Combined_tables: {combined_tables}")
     return combined_tables
 
 # LEVEL 1
@@ -107,47 +128,16 @@ def find_all_fit_table(layout): # ALL available tables instead of just 1
         if cap >= party_size:
             available_rows = all_available_row_by_key(key, layout)
             for row in available_rows:
-                available_tables.append(f'{key} - {row}') 
-    if available_tables == []: # If there's no available tables yet
-        adjacent_tables = find_fit_table_adjacent(layout, party_size)
-    else: # if there's already available tables
-        adjacent_tables = find_fit_table_adjacent(layout, party_size, available_tables)
+                available_tables.append(f'{key} - {row}')
+    new_layout = adjust_the_layout(available_tables, layout) # Create a different layout for th
+    adjacent_tables = find_fit_table_adjacent(new_layout, party_size)
     if available_tables:
         available_tables.extend(adjacent_tables) # Add a list of adjacent tables to the end available table lists 
         return available_tables  # Return a list of available tables
     else:
-        return adjacent_tables  # Placeholder for no available table
+        return adjacent_tables  # Return a list of combined tables instead
 
-# FOR FUN
-def print_available_tables(available_tables, layout): # ['T1(2) - 2', 'T2(4) - 1', 'T4(6) - 1']
-    new_layout = layout.copy()
-    for table in available_tables.copy():
-        try: 
-            row = int(table.split()[-1])
-            table_ID = table[:5] # Only take the ID e.g. T1(2) - 2 ---> T1(2)
-            col = layout[0].index(table_ID)
-            new_layout[row][col] = '*'
-        except ValueError:
-            print(table)
-            new_tables = table.split(' ')[:7] # Split the table and only take the first 7 parts of the splitted list 
-            '''
-            example: 
-                table = 'T4(6) - 6 and T5(4) - 6 could seat 10 peoples'
-                splitted_table = ['T4(6)', '-', '6', 'and', 'T5(4)', '-', '6', 'could', 'seat', '10', 'peoples']
-                new_tables = ['T4(6)', '-', '6', 'and', 'T5(4)', '-', '6'] --> only show the [:7] part        
-            '''
-            new_tables = (" ").join(new_tables)
-            new_tables = new_tables.split(' and ')
-            for table2 in new_tables:
-                row = int(table2.split()[-1])
-                table_ID = table[:5]
-                col = layout[0].index(table_ID)
-                new_layout[row][col] = '-'
-                new_layout[row][col+1] = '-'
-            print(new_tables)
-    print_layout(new_layout)
-    print("* = your option\n- = joined tables") # Additional key for user
-    print("\n=================================\n")
+
     
 # ------------------------------------------------------------------------------------
 # This second layout serves as a test case where some tables ('x') are already occupied.
@@ -171,9 +161,10 @@ print_layout(restaurant_tables2)
 available_tables = find_all_fit_table(restaurant_tables2)
 print()
 print("Available table: ")
-try: 
-    for table in available_tables:
-        print(" * " + table) # Print table with a space in front
-    print_available_tables(available_tables, restaurant_tables2) # Only print out the chart if there's a seat for the customer
-except ValueError:
-    print("\n=================================\n")
+ 
+for table in available_tables:
+    print(" * " + table) # Print table with a space in front
+new_layout = adjust_the_layout(available_tables, restaurant_tables2) # Only print out the chart if there's a seat for the customer
+print_layout(new_layout)
+print("* = your option\n- = joined tables") # Additional key for user
+print("\n=================================\n")
